0cca89650c801e0533df1bd2b377f51bdba98b6e
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-01-27 21:11:05 +0100

    Creating splash screen

---
+++ app/src/main/java/com/group10/budgeter/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.group10.budgeter.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.group10.budgeter.MainActivity
+import com.group10.budgeter.R
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashScreenActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        navigateToRightActivity();
+    }
+
+    fun navigateToRightActivity(){
+        //Go to another activity after
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 5000)
+    }
+
+    fun activityToShow(){
+
+    }
+}
