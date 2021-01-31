a4251fdcb4c35d8f12e124f59dc5567fd12d0bdd
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-01-31 22:05:57 +0100

    Creating new spend activity

---
+++ app/src/main/java/com/group10/budgeter/spend/NewSpendActivity.kt
@@ -0,0 +1,18 @@
+package com.group10.budgeter.spend
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.group10.budgeter.R
+
+class NewSpendActivity: AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_intro);
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
