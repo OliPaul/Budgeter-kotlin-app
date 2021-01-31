0e0a05ab0a66b1405d58e897699c9a53be9bcbe7
sadia ingrachen <ingrachen.sadia@gmail.com>
2021-01-31 12:07:57 +0100

    home screen added

---
+++ app/src/main/java/com/group10/budgeter/accueil/HomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.group10.budgeter.accueil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.group10.budgeter.R
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
