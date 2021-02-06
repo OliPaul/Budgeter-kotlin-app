df3e9140a8ce37368715af643614ef1b9db19786
sadia ingrachen <ingrachen.sadia@gmail.com>
2021-02-06 02:16:38 +0100

    ajout ecran d'acceuil

--- app/src/main/java/com/group10/budgeter/accueil/HomeScreenActivity.kt
+++ app/src/main/java/com/group10/budgeter/accueil/HomeScreenActivity.kt
@@ -2,11 +2,16 @@ package com.group10.budgeter.accueil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.group10.budgeter.R
 
-class HomeScreenActivity : AppCompatActivity() {
+class HomeScreenActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+    }
+
+    override fun onClick(v: View?) {
+        NewSpendActivity.navigateTo(this, 15)
     }
 }
