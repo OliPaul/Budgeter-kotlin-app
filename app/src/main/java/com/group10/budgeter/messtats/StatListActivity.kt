2c45b501ac212291daf914a49a8cb7dbbeded961
sonia96 <zemsonia@hotmail.com>
2021-02-11 15:44:21 +0100

    ajout du calcul de la somme

--- app/src/main/java/com/group10/budgeter/messtats/StatListActivity.kt
+++ app/src/main/java/com/group10/budgeter/messtats/StatListActivity.kt
@@ -32,18 +32,99 @@ class StatListActivity: AppCompatActivity(), View.OnClickListener, onStatClicked
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messtat_list);
 
-        /*for (spend in spendList){
-            var sum: Double = 0.0;
-            if(spend.spendDate >= "01/01/2021" && spend.spendDate <= "31/01/2021"){
-                sum += spend.spendAmount;
-            }
-        }*/
         recyclerViewStat?.apply {
             layoutManager = LinearLayoutManager(this@StatListActivity);
             adapter = StatListAdapter(statList, this@StatListActivity);
         }
     }
 
+    fun getSpendData(){
+        val database = Firebase.database;
+        val myRef = database.getReference("spend");
+        var spendList: MutableList<Spend> = mutableListOf();
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<HashMap<String, Any>>()
+                if (value != null) {
+                    for (spend in value){
+                        spendList.add(Gson().fromJson(spend.value.toString(), Spend::class.java));
+                    }
+                    //Generate list
+                    recyclerView?.apply {
+                        layoutManager = LinearLayoutManager(this@SpendListActivity);
+                        adapter = SpendListAdapter(spendList, this@SpendListActivity);
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("List Activity", "Failed to read value.", error.toException())
+            }
+        });
+    }
+
+fun bilanMensuelle(spendList) {
+    var statList: MutableList<MesStats> = mutableListOf();
+    for (MesStats. statList){
+        for (spend in spendList) {
+            var sum: Double = 0.0;
+
+            if (spend.spendDate >= "01/01/2021" && spend.spendDate <= "31/01/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+                //stat_amount_text
+            }
+            if (spend.spendDate >= "01/02/2021" && spend.spendDate <= "28/02/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/03/2021" && spend.spendDate <= "31/03/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/04/2021" && spend.spendDate <= "30/04/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/05/2021" && spend.spendDate <= "31/05/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/06/2021" && spend.spendDate <= "30/06/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/07/2021" && spend.spendDate <= "31/07/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/08/2021" && spend.spendDate <= "31/08/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/09/2021" && spend.spendDate <= "30/09/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/10/2021" && spend.spendDate <= "31/10/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+            if (spend.spendDate >= "01/11/2021" && spend.spendDate <= "30/11/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+
+            }if (spend.spendDate >= "01/12/2021" && spend.spendDate <= "31/12/2021") {
+                sum += spend.spendAmount;
+                MesStats.statAmount = sum;
+            }
+        }
+    }
+}
    override fun onStatClicked(mesStats: MesStats?) {
        startActivity(Intent(this, StatSemaineListActivity::class.java));
    }
