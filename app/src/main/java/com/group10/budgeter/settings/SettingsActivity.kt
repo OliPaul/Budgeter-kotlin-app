6e5b2b174e42f990105bf2ad393a4c961297403b
ingrachen <ingrachen.sadia@gmail.com>
2021-02-12 00:39:25 +0100

    settings

--- app/src/main/java/com/group10/budgeter/settings/SettingsActivity.kt
+++ app/src/main/java/com/group10/budgeter/settings/SettingsActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.group10.budgeter.R
 import com.group10.budgeter.accueil.HomeScreenActivity
 import kotlinx.android.synthetic.main.activity_settings.*
@@ -20,6 +22,13 @@ class SettingsActivity : AppCompatActivity(),  View.OnClickListener{
     }
 
     override fun onClick(v: View?) {
+        val database = Firebase.database;
+        //Check field value
+        if (monthly_cap_amount.text.toString() != ""){
+
+        }
+
+
         when(v?.id){
             R.id.backToHome -> {
                 goToHome()
